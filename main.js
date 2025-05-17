13e14af88d0376c547d26fd845c1a67755d61403
Ramon Garcia <ramonsantos.g@hotmail.com>
2025-05-17 19:55:26 -0300

    Adiciona suporte para exibir versões do Node, Electron e Chrome no template

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Corrige __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
-  // Cria a janela
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      preload: path.join(__dirname, "preloader.js"),
       nodeIntegration: true,
       contextIsolation: false,
     }
@@ -25,4 +31,20 @@ function createWindow() {
 // Starta o app
 app.whenReady().then(() => {
   createWindow();
+
+  // No macOS é comum que o app continue ativo mesmo com todas as janelas fechadas
+  app.on('activate', () => {
+    // Se não houver janelas abertas, cria uma nova
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Fecha o app quando todas as janelas estão fechadas
+app.on('window-all-closed', () => {
+  // Mesma situação do macOS
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
